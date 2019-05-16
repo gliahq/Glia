5e616d89c4a42c35cf89127e590b520adde1fa3c
Vivek Joshy <daegontaven@gmail.com>
2019-05-16 19:49:13 +0530

    Setup minimal stylesheet

--- glia/app.py
+++ glia/app.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 
 from PyQt5.QtWidgets import QApplication
 
@@ -7,8 +8,17 @@ from glia.windows.main import MainWindow
 
 def start(**kwargs):
     app = QApplication(sys.argv)
+
+    # Load Theme
+    with open(Path("../resources/themes/dracula.qss")) as qss:
+        app_style = qss.read()
+    app.setStyleSheet(app_style)
+
+    # Create Main Window
     window = MainWindow()
     window.showMaximized()
+
+    # Run & Exit
     sys.exit(app.exec_())
 
 
