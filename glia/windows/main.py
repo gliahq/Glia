42ab1f9f4f3fda50e50014319ab55e82a03978c1
Vivek Joshy <daegontaven@gmail.com>
2019-05-17 18:08:31 +0530

    Shorten long import in main winder

--- glia/windows/main.py
+++ glia/windows/main.py
@@ -1,5 +1,8 @@
 from PyQt5.QtCore import QObject, Qt
-from PyQt5.QtWidgets import QMainWindow, QDockWidget, QListWidget, QWidget, QTabWidget, QStatusBar
+from PyQt5.QtWidgets import (
+    QMainWindow, QDockWidget, QListWidget, QWidget, QTabWidget,
+    QStatusBar
+)
 
 from glia.widgets.editor.tabs import EditorTabs
 from glia.widgets.jupyter import TabbedJupyterWidget
